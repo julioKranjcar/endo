0f003c0e85c15a509d95c49a8714feca28d6782b
julioKranjcar <46230909+julioKranjcar@users.noreply.github.com>
2024-09-03 08:52:20 -0600

    x

--- src/data/servicios.jsx
+++ src/data/servicios.jsx
@@ -1,3 +1,5 @@
+//import { defineConfig } from 'vite';
+
 export const servicios = [
     {
         id: 1,
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/endo/",
- // base: "/",
+  //base: "/endo/",
+  base: "/",
 })
